d3139949de404c9773b3bdd9b1a33c0fbf5cfef1
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2018-03-18 16:13:25 +0300

    Add solution for "Alphabet Cipher" kata

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,26 @@
 (ns alphabet-cipher.coder)
 
+(def ^:private codes (zipmap (map char (range (int \a) (inc (int \z))))
+                             (range 26)))
+
 (defn encode [keyword message]
-  "encodeme")
+  (->> (cycle keyword)
+       (map #(char (+ 97 (mod (+ (codes %1) (codes %2)) 26))) message)
+       (apply str)))
 
 (defn decode [keyword message]
-  "decodeme")
+  (->> (cycle keyword)
+       (map #(char (+ 97 (mod (- (codes %1) (codes %2)) 26))) message)
+       (apply str)))
+
+(defn- extract-keyword [string]
+  (loop [n 1]
+    (if (not-every? true? (map = string (drop n string)))
+      (recur (inc n))
+      (.substring string 0 n))))
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  (->> cipher
+       (map #(char (+ 97 (mod (- (codes %2) (codes %1)) 26))) message)
+       (apply str)
+       extract-keyword))
