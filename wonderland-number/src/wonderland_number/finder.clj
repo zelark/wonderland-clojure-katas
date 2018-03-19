d4cb2a9301d159b89d87a4ff2a989675db8eb0cb
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2018-03-19 13:57:52 +0300

    Add solution for "Wonderland Number" kata

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,12 @@
 (ns wonderland-number.finder)
 
+(defn have-same-digits? [& coll]
+  "Returns true if all numbers in coll have the same digits."
+  (->> (map (comp sort str) coll)
+       (apply =)))
+
+(defn wonderland-number? [n]
+  (every? #(have-same-digits? n (* n %)) (range 2 7)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (some #(when (wonderland-number? %) %) (range 100000 (/ 1000000 6))))
