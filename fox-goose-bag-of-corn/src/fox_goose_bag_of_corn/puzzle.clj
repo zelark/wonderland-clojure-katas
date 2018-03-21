d3ebda544fc46d9a9fd0439cae124682c7f59830
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2018-03-21 23:35:21 +0300

    Add solution to "Fox, Goose and Bag of Corn" kata

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,36 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(def start-pos [[[:fox :goose :corn :you] [:boat] []]])
+(def start-pos [[:fox :goose :corn :you] [:boat] []])
+
+(def end-pos   [[] [:boat] [:fox :goose :corn :you]])
 
 (defn river-crossing-plan []
-  start-pos)
+  [(first start-pos)
+
+   [[:fox :corn] [:boat :goose :you] []]
+   [[:fox :corn] [:boat] [:you :goose]]
+  
+   [[:fox :corn] [:boat :you] [:goose]]
+   [[:fox :corn :you] [:boat] [:goose]]
+  
+   [[:corn] [:boat :you :fox] [:goose]]
+   [[:corn] [:boat] [:you :fox :goose]]
+  
+   [[:corn] [:boat :you :goose] [:fox]]
+   [[:corn :you :goose] [:boat] [:fox]]
+  
+   [[:goose] [:boat :corn :you] [:fox]]
+   [[:goose] [:boat] [:fox :corn :you]]
+  
+   [[:goose] [:boat :you] [:fox :corn]]
+   [[:goose :you] [:boat] [:fox :corn]]
+  
+   [[] [:boat :goose :you] [:fox :corn]]
+   [[] [:boat] [:fox :corn :goose :you]]])
+
+
+ :corn :you
+
+;; not-valid! :fox :goose without :you
+;; not-valid! :goose :corn without :you
+
