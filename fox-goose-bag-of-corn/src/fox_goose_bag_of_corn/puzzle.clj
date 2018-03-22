96bec345c5143c7508186ea91926e1d84f943038
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2018-03-22 16:04:21 +0300

    Update solution to "Fox, Goose and Bag of Corn" kata

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,36 +1,48 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(def start-pos [[:fox :goose :corn :you] [:boat] []])
+(defonce world (atom {}))
 
-(def end-pos   [[] [:boat] [:fox :goose :corn :you]])
+(def loc-id { :left-side 0
+              :boat 1
+              :right-side 2 })
+
+(def blank-step [[] [:boat] []])
+
+(defn render [state]
+  (reduce (fn [step {:keys [name loc]}]
+            (update step (get loc-id loc) conj name))
+          blank-step
+          (vals state)))
+
+(defn init [state]
+  (reset! state
+          { :you   { :name :you   :loc :left-side }
+            :fox   { :name :fox   :loc :left-side }
+            :goose { :name :goose :loc :left-side }
+            :corn  { :name :corn  :loc :left-side }})
+  (list (render @state)))
+
+(defn move-to [new-loc names]
+  (let [new-world (reduce #(assoc %1 %2 {:loc new-loc}) {} names)]
+    (swap! world #(merge-with merge % new-world)))
+  (render @world))
+
+(defn to-left-side [names]
+  (map #(move-to % names) [:boat :left-side]))
+
+(defn to-right-side [names]
+  (map #(move-to % names) [:boat :right-side]))
+
+(def steps [
+  [#'init          world]
+  [#'to-right-side [:you :goose]]
+  [#'to-left-side  [:you]]
+  [#'to-right-side [:you :fox]]
+  [#'to-left-side  [:you :goose]]
+  [#'to-right-side [:you :corn]]
+  [#'to-left-side  [:you]]
+  [#'to-right-side [:you :goose]]])
 
 (defn river-crossing-plan []
-  [(first start-pos)
-
-   [[:fox :corn] [:boat :goose :you] []]
-   [[:fox :corn] [:boat] [:you :goose]]
-  
-   [[:fox :corn] [:boat :you] [:goose]]
-   [[:fox :corn :you] [:boat] [:goose]]
-  
-   [[:corn] [:boat :you :fox] [:goose]]
-   [[:corn] [:boat] [:you :fox :goose]]
-  
-   [[:corn] [:boat :you :goose] [:fox]]
-   [[:corn :you :goose] [:boat] [:fox]]
-  
-   [[:goose] [:boat :corn :you] [:fox]]
-   [[:goose] [:boat] [:fox :corn :you]]
-  
-   [[:goose] [:boat :you] [:fox :corn]]
-   [[:goose :you] [:boat] [:fox :corn]]
-  
-   [[] [:boat :goose :you] [:fox :corn]]
-   [[] [:boat] [:fox :corn :goose :you]]])
-
-
- :corn :you
-
-;; not-valid! :fox :goose without :you
-;; not-valid! :goose :corn without :you
+  (mapcat (fn [[f arg]] (if (some? arg) (f arg) (f))) steps))
 
