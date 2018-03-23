3545a6870bdfe4b9305c30a221bfbc2c2c9ed273
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2018-03-24 01:10:02 +0300

    Update solution to "Fox, Goose and Bag of Corn" kata

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,31 +1,34 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(defonce world (atom {}))
+(defonce ^:private world (atom {}))
 
-(def loc-id { :left-side 0
-              :boat 1
-              :right-side 2 })
+(def ^:private loc-id
+  { :left-side 0
+    :boat 1
+    :right-side 2 })
 
-(def blank-step [[] [:boat] []])
+(def ^:private blank-step [[] [:boat] []])
 
-(defn render [state]
-  (reduce (fn [step {:keys [name loc]}]
-            (update step (get loc-id loc) conj name))
-          blank-step
-          (vals state)))
+(defn update-step [step [name pos]]
+  (update step (get loc-id pos) conj name))
 
-(defn init [state]
-  (reset! state
-          { :you   { :name :you   :loc :left-side }
-            :fox   { :name :fox   :loc :left-side }
-            :goose { :name :goose :loc :left-side }
-            :corn  { :name :corn  :loc :left-side }})
-  (list (render @state)))
+(defn render-step [state]
+  (reduce update-step blank-step @state))
 
-(defn move-to [new-loc names]
-  (let [new-world (reduce #(assoc %1 %2 {:loc new-loc}) {} names)]
-    (swap! world #(merge-with merge % new-world)))
-  (render @world))
+(defn- init []
+  (reset! world { :you   :left-side
+                  :fox   :left-side
+                  :goose :left-side
+                  :corn  :left-side })
+  (list (render-step world)))
+
+(defn update-position
+  [objects new-pos names]
+  (reduce #(assoc %1 %2 new-pos) objects names))
+
+(defn move-to [new-pos names]
+  (swap! world update-position new-pos names)
+  (render-step world))
 
 (defn to-left-side [names]
   (map #(move-to % names) [:boat :left-side]))
@@ -34,7 +37,7 @@
   (map #(move-to % names) [:boat :right-side]))
 
 (def steps [
-  [#'init          world]
+  [#'init]
   [#'to-right-side [:you :goose]]
   [#'to-left-side  [:you]]
   [#'to-right-side [:you :fox]]
@@ -45,4 +48,3 @@
 
 (defn river-crossing-plan []
   (mapcat (fn [[f arg]] (if (some? arg) (f arg) (f))) steps))
-
