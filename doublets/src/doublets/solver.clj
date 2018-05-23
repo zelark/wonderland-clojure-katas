d651c57b02710e050f4a1f42fafe3aef0db6278e
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2018-05-23 22:50:47 +0300

    Add solution to "Doublets" kata

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,18 @@
                (slurp)
                (read-string)))
 
+(defn distace [word1 word2]
+  (reduce + (map #(if (= %1 %2) 0 1) word1 word2)))
+
 (defn doublets [word1 word2]
-  "make me work")
+  (if (not= (count word1) (count word2))
+    []
+    (let [length (count word1)
+          dict (->> words (filter #(= (count %) length)) (reduce #(assoc %1 %2 (distace %2 word2)) {}))]
+      (loop [path (vector word1) word word1]
+        (let [ways (filter #(= (distace word (key %)) 1) dict)
+              next-word (->> ways (sort-by val <) (filter #((complement contains?) (set path) (key %))) first)]
+          (if (= (val next-word) 0)
+            (conj path (key next-word))
+            (recur (conj path (key next-word)) (key next-word))))))))
+
