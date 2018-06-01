af5930e907bf5798d470eb51b4ff03e6e042e9d7
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2018-06-01 13:16:01 +0300

    Update solution to "Fox, Goose and Bag of Corn" kata

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,50 +1,54 @@
-(ns fox-goose-bag-of-corn.puzzle)
+(ns fox-goose-bag-of-corn.puzzle
+  (:require [clojure.set :as set]
+            [astar.core :as astar]))
 
-(defonce ^:private world (atom {}))
 
-(def ^:private loc-id
-  { :left-side 0
-    :boat 1
-    :right-side 2 })
+(def start [#{ :fox :goose :corn :you }  #{ :boat }  #{}                        ])
+(def goal  [#{}                          #{ :boat }  #{ :fox :goose :corn :you }])
 
-(def ^:private blank-step [[] [:boat] []])
 
-(defn update-step [step [name pos]]
-  (update step (get loc-id pos) conj name))
+(defn not-bad? [[pair _]]
+  (let [bad-pairs [#{ :goose :fox  }
+                   #{ :goose :corn }]]
+    (not-any? #(set/subset? % pair) bad-pairs)))
 
-(defn render-step [state]
-  (reduce update-step blank-step @state))
 
-(defn- init []
-  (reset! world { :you   :left-side
-                  :fox   :left-side
-                  :goose :left-side
-                  :corn  :left-side })
-  (list (render-step world)))
+(defn to-boat [side]
+  (let [side (disj side :you)
+        boat #{ :boat :you }
+        others (for [item side] [(disj side item) (conj boat item)])]
+    (filter not-bad? (concat [[side boat]] others))))
 
-(defn update-position
-  [objects new-pos names]
-  (reduce #(assoc %1 %2 new-pos) objects names))
 
-(defn move-to [new-pos names]
-  (swap! world update-position new-pos names)
-  (render-step world))
+(defn leave-boat [[left boat right]]
+  (let [move-to (fn [side] (disj (into side boat) :boat))]
+    [[(move-to left) #{ :boat } right          ]
+     [left           #{ :boat } (move-to right)]]))
 
-(defn to-left-side [names]
-  (map #(move-to % names) [:boat :left-side]))
 
-(defn to-right-side [names]
-  (map #(move-to % names) [:boat :right-side]))
+(defn leave-left-side [[left _ right]]
+  (map #(vector (first %) (second %) right) (to-boat left)))
 
-(def steps [
-  [#'init]
-  [#'to-right-side [:you :goose]]
-  [#'to-left-side  [:you]]
-  [#'to-right-side [:you :fox]]
-  [#'to-left-side  [:you :goose]]
-  [#'to-right-side [:you :corn]]
-  [#'to-left-side  [:you]]
-  [#'to-right-side [:you :goose]]])
+
+(defn leave-right-side [[left _ right]]
+  (map #(vector left (second %) (first %)) (to-boat right)))
+
+
+(defn graph [[left boat right :as step]]
+  (cond
+    (some #{ :you } left)  (leave-left-side step)
+    (some #{ :you } boat)  (leave-boat step)
+    (some #{ :you } right) (leave-right-side step)))
+
+
+(defn dist [from to]
+  (count (set/difference (last to) (last from))))
+
+
+(defn get-h [goal]
+  (fn [node] (dist node goal)))
+
 
 (defn river-crossing-plan []
-  (mapcat (fn [[f arg]] (if (some? arg) (f arg) (f))) steps))
+  (map (partial map vec)
+       (cons start (astar/route graph dist (get-h goal) start goal))))
